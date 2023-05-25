5637d0e8246fc5a9939f4f8751404e404cf74960
hyunjun9788 <namhyunjun9788@gmail.com>
2023-05-25 22:30:57 +0900

    fix: disabled 적용

--- DOM_prac3.js
+++ DOM_prac3.js
@@ -6,12 +6,26 @@ const nextButton = document.querySelector('#next')
 const animalElement  = document.querySelector('#animalName')
 
 
-previousButton.addEventListener('click',()=>{
-    currentIndex = (currentIndex -1 + animals.length)%animals.length;
-    animalElement.textContent = animals[currentIndex]
-})
+previousButton.addEventListener('click',()=> {
+        currentIndex = (currentIndex - 1 + animals.length) % animals.length;
+        animalElement.textContent = animals[currentIndex]
+    if(currentIndex===0){
+        previousButton.disabled = true;
+    }
+    if(currentIndex>=1){
+        nextButton.disabled = false
+    }
+    }
+)
 
 nextButton.addEventListener('click',()=>{
     currentIndex = (currentIndex +1)%animals.length
     animalElement.textContent = animals[currentIndex];
+
+    if(currentIndex>=1){
+        previousButton.disabled=false;
+    }
+    if(currentIndex === animals.length-1){
+        nextButton.disabled=true
+    }
 })
